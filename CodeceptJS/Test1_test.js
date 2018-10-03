94606ce05a0e5c6103361aafc489f1ce3cf0b627
Zeynep Dinç <zeynepdinc91@outlook.com>
2018-10-03 22:54:47 +0300

    Test1 is updated. Variable names are arranged.

--- CodeceptJS/Test1_test.js
+++ CodeceptJS/Test1_test.js
@@ -2,11 +2,14 @@
 Feature('Test1');
 
 Scenario('home', async (I,homePage) => {
-	I.amOnPage('/');
+	
+    I.amOnPage('/');
     homePage.seeHeader();
     homePage.seeContainer();
-    let a = await I.grabNumberOfVisibleElements('//*[@id="container"]/div[3]/div/div[2]/div[3]/ul/li');
-    if(a == 56)
+
+    let numberOfImage = await I.grabNumberOfVisibleElements('//*[@id="container"]/div[3]/div/div[2]/div[3]/ul/li');
+
+    if( numberOfImage == 56 )
     {
     	console.log("    PASS");
     }
@@ -14,6 +17,7 @@ Scenario('home', async (I,homePage) => {
     {
 		console.log("    FAIL");
     }
+    
     homePage.seeLeftMenu();
 
 });
--- CodeceptJS/pages/home.js
+++ CodeceptJS/pages/home.js
@@ -3,43 +3,43 @@ const I = actor();
 
 module.exports = {
 
-	fields: {
+	fields: 
+  {
     logo: 'body > div.header-container > div > p > a',
-    aramakutusu: '#searchText',
-    detayliArama: '#searchSuggestionForm > a',
-    giris: 'body > div.header-container > div > ul > li.login-text > a',
-    uyeol: 'body > div.header-container > div > ul > li.register-text > a',
-    ilan:'#post-new-classified',
-    anasayfaVitrini: '#container > div.homepage > div > div.homepage-content > div.uiBox.showcase > h3',
-    vasita: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > a',
-    otomobil: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > ul > li:nth-child(1) > a',
-    arazi: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > ul > li:nth-child(2) > a',
-    motorsiklet: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > ul > li:nth-child(3) > a'
+    searchText: '#searchText',
+    detailedSearch: '#searchSuggestionForm > a',
+    login: 'body > div.header-container > div > ul > li.login-text > a',
+    register: 'body > div.header-container > div > ul > li.register-text > a',
+    advertisement:'#post-new-classified',
+    mainPageDisplayWindow: '#container > div.homepage > div > div.homepage-content > div.uiBox.showcase > h3',
+    vehicle: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > a',
+    car: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > ul > li:nth-child(1) > a',
+    land: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > ul > li:nth-child(2) > a',
+    motorcycles: '#container > div.homepage > div > aside > div:nth-child(1) > nav > ul.categories-left-menu > li.category-3517 > ul > li:nth-child(3) > a'
   },
 
-  // introducing methods
+  //logo,arama kutusu,detaylı arama bağlantısı, giriş, üye ol, ücretsiz ilan ver kontrolü
   seeHeader()
   {
-	I.seeElement(this.fields.logo);
-	I.seeElement(this.fields.aramakutusu);
-	I.seeElement(this.fields.detayliArama);
+  	I.seeElement(this.fields.logo);
+  	I.seeElement(this.fields.searchText);
+  	I.seeElement(this.fields.detailedSearch);
 
-	I.seeElement(this.fields.giris);
-	I.seeElement(this.fields.uyeol);
-	I.seeElement(this.fields.ilan);
+  	I.seeElement(this.fields.login);
+  	I.seeElement(this.fields.register);
+  	I.seeElement(this.fields.advertisement);
   },
+  // anasayfa vitrini kontrolü
   seeContainer()
   {
-    I.seeElement(this.fields.anasayfaVitrini);
+    I.seeElement(this.fields.mainPageDisplayWindow);
   },
+  //Vasıta başlığının ve altında Otomobil, Arazi, SUV & Pick-up, Motosiklet başlıkları kontrolü
   seeLeftMenu()
   {
-    I.seeElement(this.fields.vasita);
-    I.seeElement(this.fields.otomobil);
-    I.seeElement(this.fields.arazi);
-    I.seeElement(this.fields.motorsiklet);
-
+    I.seeElement(this.fields.vehicle);
+    I.seeElement(this.fields.car);
+    I.seeElement(this.fields.land);
+    I.seeElement(this.fields.motorcycles);
   }
-
-  
 }
