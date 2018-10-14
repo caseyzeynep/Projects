df9b49a07578c70da3890e2caf8f052968cb5818
Zeynep Dinç <zeynepdinc91@outlook.com>
2018-10-14 11:45:08 +0300

    FirsbyJs comments are fixed. Explanations are added in readme file.

--- FrisbyJS/__tests__/api/Test1.js
+++ FrisbyJS/__tests__/api/Test1.js
@@ -2,35 +2,30 @@ const frisby = require('frisby');
 
 const Joi = frisby.Joi;
 
-
-it('Country Test', function () {
+it('Country Test', function () 
+{
   return frisby.get('http://services.groupkt.com/country/get/all')
     .expect('status', 200)
-
     .expect('header', 'content-type', /application\/json/)
     .expect('jsonTypes', 'RestResponse.result.?', {})
-    .then(function (res) {
-        expect(res.json.RestResponse.result.length).toEqual(249)
+    .expect('json', 'RestResponse.result.?', { name : "Turkey", alpha2_code : "TR",alpha3_code : "TUR"})
+    .then(function (res) 
+    { 
+        expect(res.json.RestResponse.result.length).toEqual(249) 
         var sortedData = new Array(res.json.RestResponse.result.length);
         let i;
-        for( i = 0; i <res.json.RestResponse.result.length; i++)
+
+        for( i = 0; i < res.json.RestResponse.result.length; i++)
         {
             sortedData[i] = res.json.RestResponse.result[i];
         }
-        sortedData.sort(function (a, b) {
-        return a.name.localeCompare(b.name);
-        });
-        for( i = 0; i <res.json.RestResponse.result.length; i++)
+
+        sortedData.sort(function (a, b) 
         {
-            console.log(sortedData[i].name + " " + res.json.RestResponse.result[i].name);
-        }
+            return a.name.localeCompare(b.name);
+        });
+
         expect(res.json.RestResponse.result).toEqual(sortedData); 
       })
-
-    .expect('json', 'RestResponse.result.?', {name : "Turkey"})
-    .expect('json', 'RestResponse.result.?', {alpha2_code : "TR"})
-    .expect('json', 'RestResponse.result.?', {alpha3_code : "TUR"})
-
-
- });
+});
 
--- FrisbyJS/__tests__/api/Test2.js
+++ FrisbyJS/__tests__/api/Test2.js
@@ -2,24 +2,21 @@ const frisby = require('frisby');
 
 const Joi = frisby.Joi;
 
-
-it('USA Test', function () {
+it('USA Test', function () 
+{
   return frisby.get('http://services.groupkt.com/state/get/USA/all')
-
     .expect('status', 200)
     .expect('header', 'content-type', /application\/json/)
     .expect('jsonTypes', 'RestResponse.result.?', {})
- 	  .expect('jsonTypes', 'RestResponse.result.?', { 
+ 	  .expect('jsonTypes', 'RestResponse.result.?', 
+    { 
       'id': Joi.number().required(),
       'country': Joi.string().required(),
       'name': Joi.string().required(),
       'abbr': Joi.string().required(),
       'area': Joi.string().required(),
       'capital': Joi.string().required(),
-    })
-    .expect('jsonTypes', 'RestResponse.result.0', { 
       'largest_city': Joi.string().required(),
-    })
-                       
+    })                       
  });
 
